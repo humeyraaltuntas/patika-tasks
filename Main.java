a2b0845d0afaedd66dfad975f3bf8d8269c58f3f
humeyraaltuntas <humeyraaltuntas34@gmail.com>
2024-08-24 18:55:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int h;
+        Scanner input = new Scanner(System.in);
+        System.out.print("h sayısını giriniz: ");
+        h = input.nextInt();
+        System.out.println(h);
+    }
+}
